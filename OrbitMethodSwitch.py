98958c8e76687fd083385b883c59f128f55f4018
Jan Bláha <blaha.j502@gmail.com>
2022-05-26 21:33:12 +0200

    Create OrbitMethodSwitch.py

---
+++ OrbitMethodSwitch.py
@@ -0,0 +1,27 @@
+bl_info = {
+    "name": "Orbit method switch",
+    "version": (1, 0),
+    "blender": (3, 0, 0),
+    "location": "View3D > Header",
+    "description": "Convenient buttons to switch between orbit methods.",
+    "category": "Interface",
+}
+
+import bpy
+
+def toggle_orbit_method(self, context):
+    layout = self.layout
+    inputs = bpy.context.preferences.inputs
+    
+    row = layout.row(align=True)
+    row.prop_enum(inputs, "view_rotate_method", "TURNTABLE", text="", icon="ORIENTATION_GLOBAL")
+    row.prop_enum(inputs, "view_rotate_method", "TRACKBALL", text="", icon="ORIENTATION_GIMBAL")
+
+def register():
+    bpy.types.VIEW3D_HT_header.append(toggle_orbit_method)
+
+def unregister():
+    bpy.types.VIEW3D_HT_header.remove(toggle_orbit_method)
+
+if __name__ == "__main__":
+    register()
